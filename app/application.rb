695b825d5d1305fcd43e66e1e8fa189e81a11d0e
Emily Zhao <tong.zhao.92@gmail.com>
2018-05-01 17:46:18 +0000

    automatically backed up by learn

--- app/application.rb
+++ app/application.rb
@@ -25,7 +25,7 @@ class Application
     elsif req.path.match(/add/)
       if @@items.include?(req.params["item"])
         @@cart << req.params["item"]
-        resp.write "added #{reg.params["item"]}"
+        resp.write "added #{req.params["item"]}"
       else
         resp.write "We don't have that item"
       end
