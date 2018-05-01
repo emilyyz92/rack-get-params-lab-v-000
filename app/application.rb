59d55e9e6aed1a1dc801bd2f6149edfdf5b79de6
Emily Zhao <tong.zhao.92@gmail.com>
2018-05-01 17:39:20 +0000

    automatically backed up by learn

--- app/application.rb
+++ app/application.rb
@@ -23,6 +23,7 @@ class Application
         end
       end
     elsif req.path.match(/add/)
+      if @@items.include?(reg.params["item"])
       @@cart << req.params["item"]
     else
       resp.write "Path Not Found"
